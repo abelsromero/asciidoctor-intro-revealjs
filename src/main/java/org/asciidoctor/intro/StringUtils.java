9be5d7f91adc2a637e6bba1c7c547f0a519ed0c9
Abel Salgado Romero <abelromero@gmail.com>
2018-09-20 18:09:31 +0200

    initial release

---
+++ src/main/java/org/asciidoctor/intro/StringUtils.java
@@ -0,0 +1,9 @@
+package org.asciidoctor.intro;
+
+public class StringUtils {
+    // tag::contains[]
+    public boolean contains(String haystack, String needle) {
+        return haystack.contains(needle);
+    }
+    // end::contains[]
+}
